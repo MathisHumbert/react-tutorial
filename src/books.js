66a74228d67770c421d10eb33f4854675ee10e47
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-23 16:11:18 +0100

    basic react tuto done

---
+++ src/books.js
@@ -0,0 +1,17 @@
+const books = [
+  {
+    id: 1,
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_ML2_.jpg',
+    title: 'I Love You to the Moon and Backssss',
+    author: 'Amelia Hepworth',
+  },
+
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen ',
+  },
+];
+
+export default books;
--- src/index.js
+++ src/index.js
@@ -5,46 +5,18 @@ import ReactDom from 'react-dom';
 // CSS
 import './index.css';
 
-// setup vars
-const firstBook = {
-  img: 'https://images-eu.ssl-images-amazon.com/images/I/517h-u1AQlL._SX218_BO1,204,203,200_QL40_ML2_.jpg',
-  title: 'I Love You to the Moon and Backssss',
-  author: 'Amelia Hepworth',
-};
-
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-  title: 'Our Class is a Family',
-  author: 'Shannon Olsen ',
-};
+import books from './books';
+import Book from './Book';
 
 // stateless functional component
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
-
-const Book = (props) => {
-  console.log(props);
-  return (
-    <article className="book">
-      <img src={props.img} alt="" />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
-    </article>
-  );
-};
 
 ReactDom.render(<BookList />, document.getElementById('root'));
